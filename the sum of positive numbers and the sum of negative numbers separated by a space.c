c0fc93f3f1a9b03098acafc3522fd70a29f800bb
Md. Rahul Islam Joy <81201194+rahul-joy@users.noreply.github.com>
2023-10-28 04:05:13 +0600

    Add files via upload

--- the sum of positive numbers and the sum of negative numbers separated by a space.c
+++ the sum of positive numbers and the sum of negative numbers separated by a space.c
@@ -2,22 +2,25 @@
 
 int main() {
     int N;
+    printf("Enter number of total number you want to enter: ");
     scanf("%d", &N);
 
     int values[N];
     int sum_positive = 0;
     int sum_negative = 0;
-
+    
+    printf("Enter your number in integer: ");
+    
     for (int i = 0; i < N; i++) {
         scanf("%d", &values[i]);
-        if (values[i] > 0) {
-            sum_positive += values[i];
-        } else if (values[i] < 0) {
-            sum_negative += values[i];
+            if (values[i] > 0) {
+                sum_positive += values[i];
+            } else if (values[i] < 0) {
+                sum_negative += values[i];
         }
     }
 
-    printf("%d %d\n", sum_positive, sum_negative);
+    printf("The sum of positive number is: %d \nThe sum of negative number is: %d\n", sum_positive, sum_negative);
 
     return 0;
 }
